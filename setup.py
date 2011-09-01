04013fc1b61617afa4ff23b5432ffb3ea7041b7f
Jaime Soriano Pastor <jsoriano@tuenti.com>
2011-09-01 12:30:21 +0200

    setup.py added

---
+++ setup.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2011 Tuenti Technologies
+# See LICENSE for details
+
+from distutils.core import setup
+
+setup(name='puppet-enc',
+    version='0.5',
+    description='Generic external node classifier for puppet',
+    author='Jaime Soriano Pastor',
+    author_email='jsoriano@tuenti.com',
+    url='https://github.com/jsoriano/puppet-enc',
+    license='MIT',
+    package_dir={'': 'src'},
+    packages=['puppetenc'],
+)
