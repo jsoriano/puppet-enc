3bf2927fe8ad8347547d6b8632119e8dea67ed7d
Jaime Soriano Pastor <jsoriano@tuenti.com>
2011-09-01 10:46:14 +0200

    Regular expressions support

--- src/puppetenc/enc.py
+++ src/puppetenc/enc.py
@@ -1,13 +1,24 @@
 import sys
+import re
 
 from puppetenc import models
 from puppetenc.config import Session
 
-def dump_yaml(host_name):
+def get_host(host_name):
     host = Session().query(models.Host).filter_by(name=host_name).first()
+    if host:
+        return host
+    else:
+        hosts = Session().query(models.Host).all()
+        for host in hosts:
+            if re.match(host.name, host_name):
+                return host
     if not host:
         # XXX: get default
         pass
+
+def dump_yaml(host_name):
+    host = get_host(host_name)
     return "classes:\n" + "\n".join(("  %s:" % puppetClass.name for puppetClass in host.classes))
 
 if __name__ == '__main__':
